9d7a243675c58b3ba31cfdf33e2bc6fcb3c7d8bb
Mike Kelsey <aqxauror@gmail.com>
2019-11-04 00:37:31 -0500

    First Commit

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+
+import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
+
+import {ApolloProvider} from '@apollo/react-hooks'
+import {ApolloClient} from 'apollo-client';
+import {createHttpLink} from 'apollo-link-http';
+import {setContext} from 'apollo-link-context';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:8080/'
+});
+
+const authLink = setContext((_, {headers}) => {
+    // Get auth token from local storage
+    const token = JSON.parse(localStorage.authUser).token;
+    // Return the headers to context.
+    return {
+        headers: {
+            ...headers,
+            'Content-Type': 'application/graphql',
+            authorization: token ? `Bearer ${token}` : "",
+        }
+    }
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+      <FirebaseContext.Provider value={new Firebase()}>
+        <App />
+      </FirebaseContext.Provider>
+    </ApolloProvider>,
+  document.getElementById('root'),
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
